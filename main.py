ad1048ad35a5d35b7635ad4fbb3f68de4e7fc17e
Jonathan Adithya <jo.adithya391@gmail.com>
2021-05-21 09:10:55 +0700

    make request to alpha vantage to get the daily stock prices

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
+from dotenv import load_dotenv
+import os
+import requests
+
+load_dotenv()
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
-## STEP 1: Use https://www.alphavantage.co
+# STEP 1: Use https://www.alphavantage.co
 # When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+url = 'https://www.alphavantage.co/query'
+parameters = {
+    'function': 'TIME_SERIES_DAILY',
+    'symbol': STOCK,
+    'apikey': os.environ.get('PRICE_API_KEY')
+}
 
-## STEP 2: Use https://newsapi.org
+response = requests.get(url, params=parameters)
+response.raise_for_status()
+stock_prices = response.json()['Time Series (Daily)']
+print(stock_prices)
+prices = []
+
+for i, j in enumerate(stock_prices):
+    prices.append(float(stock_prices[j]['4. close']))
+    if i == 1:
+        break
+
+# STEP 2: Use https://newsapi.org
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
 
-## STEP 3: Use https://www.twilio.com
-# Send a seperate message with the percentage change and each article's title and description to your phone number.
+# STEP 3: Use https://www.twilio.com
+# Send a separate message with the percentage change and each article's title and description to your phone number.
 
 
-#Optional: Format the SMS message like this:
+# Optional: Format the SMS message like this:
 """
 TSLA: 🔺2%
 Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
